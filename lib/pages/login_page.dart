2aab483faa5299d08639d0d3bf01a51a00d4ae95
Luis Schröder <kresstudios@gmail.com>
2024-11-18 19:00:16 +0100

    Login/register page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,68 +18,72 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-          // icon
-          Icon(Icons.lock_open_rounded, size: 60, color: Theme.of(context).colorScheme.primary),
-
-          const SizedBox(height: 50),
-
-          // welcome back , you've been missed
-          Text('Welcome back, you\'ve been missed!',
-          style: TextStyle(
-            fontSize: 16,
-            color: Theme.of(context).colorScheme.primary,
-           ),
-          ),
-
-          const SizedBox(height: 25),
-
-          // email textfield
-          MyTextfield(
-            hintText: "Enter email",
-            obscureText: false,
-            controller: _emailController,
-            ),
-
-          const SizedBox(height: 10),
-
-          // password textfield
-          MyTextfield(
-            hintText: "Enter password",
-            obscureText: true,
-            controller: _pwController,
-            ),
-
-          const SizedBox(height: 25),
-
-          // login button
-          MyButton(
-            text: "Login", 
-            onTap: login
-            ),
-
-          const SizedBox(height: 25),
-
-          // not a member? register now
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: SafeArea(
+          child: Column(
             children: [
-              Text("Not a member? ", style: TextStyle(color: Theme.of(context).colorScheme.primary),),
-              
-              GestureDetector(
-                onTap: onTap,
-                child: Text(
-                  "Register now", 
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary, 
-                    fontWeight: FontWeight.bold),
-                    )
-                   ),
+          
+              // icon
+              Icon(Icons.lock_open_rounded, size: 60, color: Theme.of(context).colorScheme.primary),
+          
+              const SizedBox(height: 50),
+          
+              // welcome back , you've been missed
+              Text('Welcome back, you\'ve been missed!',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.primary,
+               ),
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // email textfield
+              MyTextfield(
+                hintText: "Enter email",
+                obscureText: false,
+                controller: _emailController,
+                ),
+          
+              const SizedBox(height: 10),
+          
+              // password textfield
+              MyTextfield(
+                hintText: "Enter password",
+                obscureText: true,
+                controller: _pwController,
+                ),
+          
+              const SizedBox(height: 25),
+          
+              // login button
+              MyButton(
+                text: "Login", 
+                onTap: login
+                ),
+          
+              const SizedBox(height: 25),
+          
+              // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Not a member? ", style: TextStyle(color: Theme.of(context).colorScheme.primary),),
+                  
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Register now", 
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary, 
+                        fontWeight: FontWeight.bold),
+                        )
+                       ),
+                ],
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
