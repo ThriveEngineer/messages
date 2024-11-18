a8cf88e0e672c93a40eaf34f9c3af139af778e88
Luis Schröder <kresstudios@gmail.com>
2024-11-18 18:05:15 +0100

    Firebase is there

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:messages/components/themes/dark_mode.dart';
 import 'package:messages/components/themes/light_mode.dart';
+import 'package:messages/firebase_options.dart';
 import 'package:messages/services/auth/login_or_register.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
